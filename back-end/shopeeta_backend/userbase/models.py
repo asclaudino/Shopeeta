76a9a57b375c83528b7d1dab9bcc9c90cd934e26
GaGandour <gagandour@gmail.com>
2022-05-18 09:22:04 -0300

    added boolean iniciativa field to User

--- back-end/shopeeta_backend/userbase/models.py
+++ back-end/shopeeta_backend/userbase/models.py
@@ -7,6 +7,7 @@ class User(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     email = models.EmailField()
+    is_iniciativa = models.BooleanField(default=False)
 
     def __str__(self):
         return self.username
